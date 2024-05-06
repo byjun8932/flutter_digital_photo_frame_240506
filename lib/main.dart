1ad73fb760a5e7d05259c6384477b05f15bb32c8
sangjun <byjun8932@gmail.com>
2024-05-06 15:47:14 +0900

    feat. FutureBuilder로 사진 표시하기

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -52,6 +54,26 @@ class _MyGalleryAppState extends State<MyGalleryApp> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('전자액자입니다.'),
       ),
+      body: images == null
+          ? const Center(
+              child: Text('No Images'),
+            )
+          : FutureBuilder<Uint8List>(
+              future: images![0].readAsBytes(),
+              builder: (context, snapshot) {
+                final data = snapshot.data;
+
+                if (data == null ||
+                    snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+                return Image.memory(
+                  data,
+                  width: double.infinity,
+                );
+              }),
     );
   }
 }
